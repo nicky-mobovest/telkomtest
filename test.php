fcd26aa6c4d85932277bfcc58cc2b53d670a0cbb
nicky-mobovest <50163002+nicky-mobovest@users.noreply.github.com>
2020-05-08 02:24:06 +0700

    request done, need to beautify

---
+++ test.php
@@ -0,0 +1,45 @@
+<?php 
+	echo "Welcome to the telkom test ! <br><br>" ;
+?>
+<?php 
+#set up param 
+$url = "https://api.github.com" ; 
+$username = 'nicky-mobovest';
+$action = "/users/{$username}/repos" ; 
+$query = http_build_query([
+            'type' => 'all',
+            'sort' => "created" ,
+            'direction' => "asc" 
+]);
+$url .= $action . "?".$query ; 
+#set up curl
+$data = getRequest ($url); 
+
+if ($data['status'] == 1 ){
+	$list = json_decode($data['response'], true ); 
+}else {
+	echo $data['response']; 
+}
+	echo print_r($list,true ); 
+
+function getRequest ($url){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$retValue = curl_exec($ch);
+
+	if (!curl_errno($ch)) {
+		curl_close($ch);
+		return array( 'status' => 1 ,
+					  'response' => $retValue); 
+	}else {
+		curl_close($ch);
+		return array( 'status' => 2 ,
+					  'response' => "<br>Error - Bad Response <br>" . curl_getinfo($ch, CURLINFO_HTTP_CODE)); 
+	}	
+}
+
+
+?>	
